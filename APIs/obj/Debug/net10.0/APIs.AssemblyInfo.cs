4249b058cd222673db1d9f41877ced358ee48acc
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-23 13:52:51 +0100

    Will create 2 separate projects

--- APIs/obj/Debug/net10.0/APIs.AssemblyInfo.cs
+++ APIs/obj/Debug/net10.0/APIs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8e078c9079fcd08f38dbb45ec3697995d36e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b663e3a14e6cb686e9b3328f924ca74fefe63ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
